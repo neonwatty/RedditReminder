eb9feb8b11360d5134973c20ed7e9c0c4997af7c
neonwatty <jermwatt@gmail.com>
2026-04-25 05:54:09 -0700

    feat: background refresh timer and notification permission request on launch

--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -2,17 +2,51 @@ import AppKit
 
 @MainActor
 final class AppDelegate: NSObject, NSApplicationDelegate {
-  let panelController = PanelController()
-  private let globalShortcut = GlobalShortcut()
+    let panelController = PanelController()
+    let timingEngine = TimingEngine()
+    let notificationService = NotificationService()
+    let heuristicsStore = HeuristicsStore()
 
-  func applicationDidFinishLaunching(_ notification: Notification) {
-    globalShortcut.register { [weak self] in
-      self?.panelController.toggleCapture()
+    private let globalShortcut = GlobalShortcut()
+    private var refreshTimer: Timer?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Register global shortcut
+        globalShortcut.register { [weak self] in
+            self?.panelController.toggleCapture()
+        }
+
+        // Request notification permission
+        Task {
+            _ = await notificationService.requestPermission()
+        }
+
+        // Start 5-minute refresh timer
+        startRefreshTimer()
+
+        NSLog("RedditReminder: launched, ⌘⇧R registered, refresh timer started")
     }
-    NSLog("RedditReminder: launched, ⌘⇧R registered")
-  }
 
-  func applicationWillTerminate(_ notification: Notification) {
-    globalShortcut.unregister()
-  }
+    func applicationWillTerminate(_ notification: Notification) {
+        globalShortcut.unregister()
+        refreshTimer?.invalidate()
+    }
+
+    private func startRefreshTimer() {
+        refreshTimer?.invalidate()
+        refreshTimer = Timer.scheduledTimer(
+            withTimeInterval: 5 * 60,
+            repeats: true
+        ) { [weak self] _ in
+            Task { @MainActor in
+                self?.runRefreshCycle()
+            }
+        }
+        // Also run immediately on launch
+        runRefreshCycle()
+    }
+
+    private func runRefreshCycle() {
+        NSLog("RedditReminder: refresh cycle tick")
+    }
 }
