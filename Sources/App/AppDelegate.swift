77651a145eb2596f968b85387eb45342baa91c19
neonwatty <jermwatt@gmail.com>
2026-04-27 17:11:13 -0700

    fix: add @preconcurrency import for UserNotifications Sendable conformance

--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -1,6 +1,6 @@
 import AppKit
 import SwiftData
-import UserNotifications
+@preconcurrency import UserNotifications
 
 @MainActor
 final class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDelegate {
